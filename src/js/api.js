a10ec08bdcb12918f620f2535bb392dce519dc6a
Ignacio <ignacio@kasevich.com.ar>
2019-10-21 12:01:13 -0300

    [Test]

--- src/js/api.js
+++ src/js/api.js
@@ -34,7 +34,7 @@ wsServer.addEventListener('request', function (req) {
     if (token == PRIVATE_TOKEN) {
       var func = msj.func;
       var data = msj.data;
-      handleError(JSON.parse(data));
+      handleError(JSON.stringify(data));
       var funcs = {
         print: handlePrint,
         add: addPrinter,
